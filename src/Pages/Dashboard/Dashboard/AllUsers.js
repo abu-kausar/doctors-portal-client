5d3a59004d73c8b7482727c63d9020c80aead4e9
abu-kausar <abukausar33@gmail.com>
2023-03-25 15:12:18 +0600

    readme file added

--- src/Pages/Dashboard/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/Dashboard/AllUsers.js
@@ -36,8 +36,8 @@ const AllUsers = () => {
                         <th></th>
                         <th>Name</th>
                         <th>Email</th>
-                        <th>Admin</th>
-                        <th>Delete</th>
+                        <th>Action</th>
+                        {/* <th>Delete</th> */}
                     </tr>
                 </thead>
                 <tbody>
@@ -48,7 +48,7 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn text-white btn-primary'>Make Admin</button>}</td>
-                                <td><button className='btn text-white btn-danger'>Delete</button></td>
+                                {/* <td><button className='btn text-white btn-danger'>Delete</button></td> */}
                             </tr>
                         )
                     }
